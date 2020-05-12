b0c4485744686b24beab86f97dc2cd1f9fb709b3
Vincent Sanders <vincesanders@hotmail.com>
2020-05-12 09:39:47 -0500

    Nearly finished with the Binary Search Tree.

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -15,29 +15,79 @@ class BSTNode:
         self.left = None
         self.right = None
 
+class BinarySearchTree:
+    def __init__(self, value):
+        if value is None:
+            self.root = None
+        else:
+            self.root = BSTNode(value)
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        node = self.root
+        if node is None:
+            self.root = BSTNode(value)
+            return True
+        else:
+            def search_tree(node):
+                if value < node.value:
+                    if node.left is None:
+                        node.left = BSTNode(value)
+                        return True # break out of recursion
+                    else:
+                        return search_tree(node.left)
+                elif value > node.value:
+                    if node.right is None:
+                        node.right = BSTNode(value)
+                        return True
+                    else:
+                        return search_tree(node.right)
+                else:
+                    return True
+            return search_tree(node)
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        current_node = self.root
+        while current_node:
+            if target is current_node.value:
+                return True
+            elif target < current_node.value:
+                current_node = current_node.left
+            else:
+                current_node = current_node.right
+        return False
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        current_node = self.root
+        while current_node.left is not None:
+            current_node = current_node.left
+        return current_node.value
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        pass
+        if self.root is None:
+            return None
+        else:
+            return fn(self.root)
 
     # Part 2 -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
-        pass
+        def traverse_in_order(node, values=[]):
+            if node.left:
+                traverse_in_order(node.left, values)
+            values.append(node.value)
+            if node.right:
+                traverse_in_order(node.right, values)
+            return values
+        in_order_list = self.for_each(traverse_in_order)
+        for v in in_order_list:
+            print(v)
+        return in_order_list
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
@@ -54,8 +104,28 @@ class BSTNode:
 
     # Print Pre-order recursive DFT
     def pre_order_dft(self, node):
-        pass
+        def traverse_pre_order(node, values=[]):
+            values.append(node.value)
+            if node.left:
+                traverse_pre_order(node.left, values)
+            if node.right:
+                traverse_pre_order(node.right, values)
+            return values
+        pre_order_list = self.for_each(traverse_pre_order)
+        for v in pre_order_list:
+            print(v)
+        return pre_order_list
 
     # Print Post-order recursive DFT
     def post_order_dft(self, node):
-        pass
+        def traverse_post_order(node, values=[]):
+            if node.left:
+                traverse_post_order(node.left, values)
+            if node.right:
+                traverse_post_order(node.right, values)
+            values.append(node.value)
+            return values
+        post_order_list = self.for_each(traverse_post_order)
+        for v in post_order_list:
+            print(v)
+        return post_order_list
