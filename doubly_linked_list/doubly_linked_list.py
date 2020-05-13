26c856f37af1f01dc9fcf0062c872c694ec8512c
Vincent Sanders <vincesanders@hotmail.com>
2020-05-13 12:03:58 -0500

    Optimized doubly linked move to end method.

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -131,17 +131,16 @@ class DoublyLinkedList:
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
-    def move_to_end(self, node): # optimize
-        current_node = self.head
-        while current_node.next is not None:
-            if current_node == node:
-                if current_node is self.head:
-                    self.head = self.head.next
-                current_node.delete()
-                break
-            current_node = current_node.next
-        self.tail.insert_after(node.value)
-        self.tail = self.tail.next
+    def move_to_end(self, node):
+        if node is self.tail:
+            return None
+        value = node.value
+        if node is self.head:
+            self.remove_from_head()
+        else:
+            node.delete()
+            self.length -= 1
+        self.add_to_tail(value)
 
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
